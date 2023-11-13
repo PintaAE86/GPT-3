79f37b12f72f163a6e558cefb94def6791e91ea5
Parwinder Singh <131915695+PintaAE86@users.noreply.github.com>
2023-11-13 14:44:47 -0500

    created a container section

---
+++ src/container/feature/Featuer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './feature.css';
+const Featuer = () => {
+  return (
+    <div>Featuer</div>
+  )
+}
+
+export default Featuer
---
+++ src/container/possiblity/possiblity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './possiblity.css';
+const possiblity = () => {
+  return (
+    <div>possiblity</div>
+  )
+}
+
+export default possiblity
