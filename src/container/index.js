47f4521b9982e173742c54067157648b14ecf464
Parwinder Singh <131915695+PintaAE86@users.noreply.github.com>
2023-11-15 19:19:04 -0500

    added Feature containers and styling

--- src/container/index.js
+++ src/container/index.js
@@ -1,5 +1,5 @@
 export {default as Blog} from './blog/Blog.jsx';
-export {default as Feature} from './feature/Feature.jsx';
+export {default as Features} from './feature/Features.jsx';
 export {default as Footer} from './footer/Footer.jsx';
 export {default as Header} from './header/Header.jsx';
 export {default as Possiblity} from './possibility/Possibility.jsx';
